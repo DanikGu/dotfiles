4d918223e56e517c165a63f0e3352ba4e97ef49f
DanikGu <petrikpzto4kin@gmail.com>
2025-09-28 17:05:21 +0300

    Fix path to config

--- gigainstall_scripts/15-setup-ukrainian-locale.sh
+++ gigainstall_scripts/15-setup-ukrainian-locale.sh
@@ -14,7 +14,7 @@ echo "Creating Hyprland keyboard config..."
 HYPR_CUSTOM_DIR="$HOME/.config/hypr-custom"
 mkdir -p "$HYPR_CUSTOM_DIR"
 
-cat <<EOT > "$HYPR_CUSTOM_DIR/keyboard.conf"
+cat <<EOT >"$HYPR_CUSTOM_DIR/keyboard.conf"
 input {
     kb_layout = us,ua
     kb_options = grp:alt_shift_toggle
@@ -23,14 +23,14 @@ EOT
 
 # 4. Source the new config file in hypr-user.conf
 echo "Sourcing keyboard config in hypr-user.conf..."
-HYPR_USER_CONF="$HOME/dotfiles/CaelestiaCustom/hypr-user.conf"
+HYPR_USER_CONF="$HOME/.config/caelestia/hypr-user.conf"
 
 if [ -f "$HYPR_USER_CONF" ]; then
   # Add the source line if it doesn't exist
   if ! grep -q "source=~/.config/hypr-custom/keyboard.conf" "$HYPR_USER_CONF"; then
     echo -e "
 # Source keyboard configuration
-source=~/.config/hypr-custom/keyboard.conf" >> "$HYPR_USER_CONF"
+source=~/.config/hypr-custom/keyboard.conf" >>"$HYPR_USER_CONF"
   fi
 else
   echo "Warning: hypr-user.conf not found at $HYPR_USER_CONF. You will need to manually source the keyboard.conf file."
